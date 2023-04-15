ad8704d8b60fb2e244d6ecc24d84f8c8768a956c
marcelodnmachado <marcelodnmachado@gmail.com>
2023-04-15 20:04:08 -0300

    [REF] Alterando o projeto utilizando as boas praticas

--- src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
+++ src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
@@ -1,5 +1,7 @@
 package br.com.fiap.abctechapi.controller;
 
+import br.com.fiap.abctechapi.util.VersionComponent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,11 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.PrivateKey;
 import java.util.Properties;
 
 @RestController
 @RequestMapping("/")
 public class HealthCheckController {
+
+    private VersionComponent versionComponent;
+
+    public HealthCheckController(@Autowired VersionComponent versionComponent){
+        this.versionComponent = versionComponent;
+    }
 
     @GetMapping
     public ResponseEntity<String> status(){
@@ -19,13 +28,8 @@ public class HealthCheckController {
     }
 
     @GetMapping("version")
-    public ResponseEntity<String> version() throws IOException {
-        Properties properties = new Properties();
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.yml");
-
-        properties.load(inputStream);
-
-        return ResponseEntity.ok(properties.get("build_name") + " - " + properties.get("build_version"));
+    public ResponseEntity<String> version() {
+        return ResponseEntity.ok(this.versionComponent.getVersion());
 
     }
 }
